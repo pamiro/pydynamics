4505681e1fe9591aa0489ef657a7868d00919557
p.mironchyk <p.mironchyk@gmail.com>
2011-07-29 17:19:46 +0000

    Redesign archive structure

---
+++ pydynamics/joints.py
@@ -0,0 +1,61 @@
+#    Copyright (c) 2011, PyDynamics Authors
+#    All rights reserved.
+#    
+#    Redistribution and use in source and binary forms, with or without
+#    modification, are permitted provided that the following conditions are met:
+#        * Redistributions of source code must retain the above copyright
+#          notice, this list of conditions and the following disclaimer.
+#        * Redistributions in binary form must reproduce the above copyright
+#          notice, this list of conditions and the following disclaimer in the
+#          documentation and/or other materials provided with the distribution.
+#        * Neither the name of the PyDynamics nor the
+#          names of its contributors may be used to endorse or promote products
+#          derived from this software without specific prior written permission.
+#    
+#    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+#    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#    DISCLAIMED. IN NO EVENT SHALL PYDYNAMICS AUTHORS BE LIABLE FOR ANY
+#    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+'''
+Created on Jul 29, 2011
+
+@author: Pavel Mironchyk <p.mironchyk at gmail.com>
+'''
+
+import numpy    as np
+import spatvect as sv
+from model import Joint
+
+class RevoluteJoint(Joint):
+    def __init__(self, bodyA, bodyB, q = 0.0):
+        Joint.__init__(self, bodyA, bodyB)
+        self.update(q)
+    
+    def update(self, q):
+        self.Xj = sv.Xrotz(q)
+        self.S = np.matrix([0,0,1,0,0,0]).transpose()
+
+class PrismaticsJoint(Joint):
+    def __init__(self, bodyA, bodyB, d):
+        Joint.__init__(self, bodyA, bodyB)
+        self.update(d)
+    
+    def update(self, d):
+        self.Xj = sv.Xtrans(np.array[0, 0, d])
+        self.S  = np.matrix([0,0,0,0,0,1]).transpose();
+
+class HelicalJoint(Joint):
+    def __init__(self, bodyA, bodyB, q, d):
+        Joint.__init__(self, bodyA, bodyB)
+        self.update(q, d)
+    
+    def update(self, q, d):
+        self.Xj = sv.Xrotz(q) * sv.Xtrans(np.array([0, 0, q*d]));
+        self.S  = np.matrix([0, 0, 1, 0, 0, d]).transpose();
---
+++ pydynamics/model.py
@@ -0,0 +1,118 @@
+#    Copyright (c) 2011, PyDynamics Authors
+#    All rights reserved.
+#    
+#    Redistribution and use in source and binary forms, with or without
+#    modification, are permitted provided that the following conditions are met:
+#        * Redistributions of source code must retain the above copyright
+#          notice, this list of conditions and the following disclaimer.
+#        * Redistributions in binary form must reproduce the above copyright
+#          notice, this list of conditions and the following disclaimer in the
+#          documentation and/or other materials provided with the distribution.
+#        * Neither the name of the PyDynamics nor the
+#          names of its contributors may be used to endorse or promote products
+#          derived from this software without specific prior written permission.
+#    
+#    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+#    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+#    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#    DISCLAIMED. IN NO EVENT SHALL PYDYNAMICS AUTHORS BE LIABLE FOR ANY
+#    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+#    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+'''
+Created on Jul 29, 2011
+
+@author: Pavel Mironchyk <p.mironchyk at gmail.com>
+'''
+
+import numpy    as np
+import spatvect as sv
+
+class RigidBody:
+    """Rigid body with CoM and spacial Inertia """
+    def __init__(self):         
+        
+        #joints that connect body to other bodies"""
+        self.joints  = {}
+        
+        self.mass = 0.0
+        self.CoM  = np.array([0.0, 0.0, 0.0])
+        
+        # spacial rigid body inertia
+        self.rbI  = None        
+        # moment of inertia tensor
+        self.mcI  = None
+        
+        self.figure = None
+
+        
+class Joint:
+    def __init__(self, bodyA, bodyB):
+        self.bodies = [bodyA, bodyB]
+        bodyA.joints[bodyB] = self  
+              
+        # Join Transformation
+        self.Xj = None
+        # Joint Space
+        self.S = None    
+        # spacial transformation to joint in body A to body B coordinates
+        self.Xtree = sv.Xtrans([0, 0, 0])
+
+        self.figure = None
+
+class Model:
+    def __init__(self):
+        self.bodies = []
+        self.joints = [] 
+ 
+    def addbody(self, b):
+        self.bodies.append(b)
+    
+    def addjoint(self, j):
+        self.joints.append(j)
+    
+    def add(self, o):
+        if isinstance(o, RigidBody):
+            self.addbody(o)
+        if isinstance(o, Joint):
+            self.addjoint(o)
+
+class KTree:
+    """Construct a kinematics tree from a model""" 
+     
+    def __init__(self, model):
+        self.model = model
+        self.update()
+        
+    def update_parent(self):
+        self.parent = []
+        if len(self.model.bodies) > 0:
+            for bindex in range(1, len(self.model.bodies)):
+                body = self.model.bodies[bindex]
+                for joint in self.model.joints:
+                    if body in joint.bodies:        
+                        ind = (joint.bodies.index(body) + 1) % 2
+                        other_body = joint.bodies[ind]
+                        other_body_ind = self.model.bodies.index(other_body)        
+                        if other_body_ind < bindex:
+                            self.parent.append(other_body_ind)
+        return self.parent
+        
+ 
+    def update_representation(self):
+        self.predecessor = []
+        self.successor = []
+        for j in self.model.joints:
+            self.predecessor.append(self.model.bodies.index(j.bodies[0])) 
+            self.successor.append(self.model.bodies.index(j.bodies[1]))    
+            
+        return (self.predecessor, self.successor)
+
+    def update(self):
+        self.update_parent()
+        self.update_representation
+
